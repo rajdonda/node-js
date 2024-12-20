443d81ed6985802e4ac973da9376ac779bdff922
raj donda <155087919+rajdonda@users.noreply.github.com>
2024-12-20 20:09:25 +0530

    prac-4

--- prac-4/index.js
+++ prac-4/index.js
@@ -1,10 +1,47 @@
 const http = require("http");
+const fs = require('fs');
 
-const port = 8000;
+const port = 3000;
 
 const app = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World");
+    let fileName = "";
+
+    switch (req.url) {
+        case '/':
+            fileName = 'Home.html';
+            break;
+        case '/about':
+            fileName = 'About.html';
+            break;
+        case '/login':
+            fileName = 'Login.html';
+            break;
+        case '/header':
+            fileName = 'header.html';
+            break;
+        case '/read':
+            fileName = 'read.html';
+            break;
+        case '/cart':
+            fileName = 'cart.html';
+            break;
+        case '/review':
+            fileName = 'review.html';
+            break;
+        default:
+            fileName = 'contact.html';
+    }
+
+    fs.readFile(fileName, (err, pagename) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end('<h1>404 - Page Not Found</h1>');
+            console.error(`Error reading file: ${fileName}.`, err);
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(pagename);
+        }
+    });
 });
 
 app.listen(port, (err) => {
