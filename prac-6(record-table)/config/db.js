e0b122d42c601e8b39f8e710da98962942173326
raj donda <155087919+rajdonda@users.noreply.github.com>
2025-02-06 19:43:04 +0530

    pr-5

---
+++ prac-6(record-table)/config/db.js
@@ -0,0 +1,8 @@
+mongoose.connect('mongodb://localhost:27017/recordDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('MongoDB connected successfully');
+}).catch(err => {
+    console.error('MongoDB connection error:', err);
+});
